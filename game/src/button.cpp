70408dfe3b7aba473f15800ffbc17089611cd3db
Emanuel <manusenac@hotmail.com>
2017-08-25 21:32:45 -0300

    Update button.cpp
    alteração de  nomes de variáveis para adequar a folha de estilo

--- game/src/button.cpp
+++ game/src/button.cpp
@@ -1,19 +1,19 @@
 #include "button.hpp"
 
-Button::Button(std::string objectName, double positionX, double positionY,
+Button::Button(std::string objectName, double position_x, double position_y,
                                      int width, int height, std::string title, int sizeTitle) : GameObject(objectName,
-                                                                         positionX,
-                                                                         positionY,
+                                                                         position_x,
+                                                                         position_y,
                                                                          width, height){
 
-    text = new Text(title, objectName, sizeTitle, true, new Color(0,0,0,0), new Color(0,0,0,0));
+    text = new Text(title, object_name, size_title, true, new Color(0,0,0,0), new Color(0,0,0,0));
 }
 
 Button::~Button(){}
 
-void Button::update(double timeElapsed){
-    timeElapsed = timeElapsed;
-    INFO(timeElapsed);
+void Button::update(double time_elapsed){
+    time_elapsed = time_elapsed;
+    INFO(time_elapsed);
 
 }
 
@@ -22,8 +22,8 @@ void Button::draw(){
     text->draw(getPositionX(), getPositionY());
 }
 
-void Button::setTextColor(Color * newTextColor){
-    text->setTextColor(newTextColor);
+void Button::setTextColor(Color * new_text_color){
+    text->setTextColor(new_text_color);
 }
 
 Text * Button::getText(){
