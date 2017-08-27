9431784699d32239ffea5f21afec8ca209996aa0
joao <joao_salibaa@hotmail.com>
2017-08-27 14:24:12 -0300

    style sheet in window_manager

--- engine/src/window_manager.cpp
+++ engine/src/window_manager.cpp
@@ -1,66 +1,66 @@
 #include "window_manager.hpp"
 using namespace engine;
 
-SDL_Window *gameWindow;
-SDL_Renderer *gameCanvas;
+SDL_Window *game_window;
+SDL_Renderer *game_canvas;
 
-bool WindowManager::createWindow(std::string windowTitle, std::pair<int, int> windowSize){
+bool WindowManager::createWindow(std::string window_title, std::pair<int, int> window_size) {
     INFO("Create window");
-    gameWindow = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED,
-    SDL_WINDOWPOS_CENTERED, windowSize.first,
-    windowSize.second, SDL_WINDOW_SHOWN);
+    game_window = SDL_CreateWindow(window_title.c_str(), SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, window_size.first,
+    window_size.second, SDL_WINDOW_SHOWN);
 
-    if(gameWindow == NULL){
+    if(game_window == NULL) {
         return false;
     }
 
     INFO("Create canvas");
-    gameCanvas = SDL_CreateRenderer(gameWindow, -1,
+    game_canvas = SDL_CreateRenderer(game_window, -1,
         SDL_RENDERER_ACCELERATED);
 
-        if(gameCanvas == NULL){
+        if(game_canvas == NULL) {
             return false;
         }
 
-        SDL_SetRenderDrawColor(gameCanvas, 100, 100, 100, 255);
-        SDL_RenderClear(gameCanvas);
-        SDL_RenderPresent(gameCanvas);
+        SDL_SetRenderDrawColor(game_canvas, 100, 100, 100, 255);
+        SDL_RenderClear(game_canvas);
+        SDL_RenderPresent(game_canvas);
         setImageIcon();
         return true;
     }
 
 
-    bool WindowManager::destroyWindow(){
+    bool WindowManager::destroyWindow() {
         INFO("Destroying canvas");
-        SDL_DestroyRenderer(gameCanvas);
-        gameCanvas = NULL;
+        SDL_DestroyRenderer(game_canvas);
+        game_canvas = NULL;
 
         INFO("Destroying window");
-        SDL_DestroyWindow(gameWindow);
-        gameWindow = NULL;
+        SDL_DestroyWindow(game_window);
+        game_window = NULL;
 
         return true;
     }
 
-    SDL_Window* WindowManager::GetGameWindow(){
-        return gameWindow;
+    SDL_Window* WindowManager::GetGameWindow() {
+        return game_window;
     }
 
 
-    SDL_Renderer* WindowManager::getGameCanvas(){
-        return gameCanvas;
+    SDL_Renderer* WindowManager::getGameCanvas() {
+        return game_canvas;
     }
     
-    void WindowManager::setImageIcon(){
+    void WindowManager::setImageIcon() {
       // TODO Change image icon.
       SDL_Surface * image = NULL;
       image = IMG_Load(std::string("assets/sprites/alien.png").c_str());
 
-      if(image == NULL){
+      if(image == NULL) {
             exit(-1);
       }
 
-      SDL_SetWindowIcon(gameWindow, image);
+      SDL_SetWindowIcon(game_window, image);
       SDL_FreeSurface(image);
 
     }
