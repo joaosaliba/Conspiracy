6df2da6886e2692a284e2421be3e9f93e647f063
Daniel Oda <danieloda35@gmail.com>
2017-08-27 17:35:37 -0300

    Applying stylesheet 4

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -5,65 +5,65 @@ namespace engine{
     const std::string GAME_NAME = "Conspiracy";
     const std::pair <int, int> WINDOW_SIZE (960, 640);
 
-    SceneManager* sceneManager;
-    WindowManager* windowManager;
-    SDLManager* sdlManager;
-    double startTime;
-    double stepTime;
-    double timeElapsed;
-    double frameTime;
-    double frameRate = 60.0;
-    SceneManager* getSceneManager(){
-        return sceneManager;
+    SceneManager* scene_manager;
+    WindowManager* window_manager;
+    SDLManager* sdl_manager;
+    double start_time;
+    double step_time;
+    double time_elapsed;
+    double frame_time;
+    double frame_rate = 60.0;
+    SceneManager* getSceneManager() {
+        return scene_manager;
     }
-    void loadEngine(){
-        sceneManager = new SceneManager();
-        windowManager = new WindowManager();
-        sdlManager = new SDLManager();
-        startTime = SDL_GetTicks();
-        stepTime = startTime;
-        frameTime = 1000.0/frameRate;
+    void loadEngine() {
+        scene_manager = new SceneManager();
+        window_manager = new WindowManager();
+        sdl_manager = new SDLManager();
+        start_time = SDL_GetTicks();
+        step_time = start_time;
+        frame_time = 1000.0/frame_rate;
 
-        if(!sdlManager->initSDL()){
+        if(!sdl_manager->initSDL()) {
             ERROR("ERRO AO INICIAR SDL");
             exit(-1);
-        }else if(!windowManager->createWindow(GAME_NAME, WINDOW_SIZE)){
+        }else if(!window_manager->createWindow(GAME_NAME, WINDOW_SIZE)) {
             ERROR("ERRO AO CRIAR JANELA");
             exit(-1);
         }
     }
 
-    void run(){
-        bool isRunning = true;
+    void run() {
+        bool is_running = true;
         SDL_Event event;
 
-        while(isRunning){
-            stepTime = SDL_GetTicks();
+        while(is_running){
+            step_time = SDL_GetTicks();
 
             engine::InputManager::instance.update(event);
             SDL_RenderClear(WindowManager::getGameCanvas());
 
-            if(engine::InputManager::instance.getQuitRequest()){
-                isRunning = false;
-                sdlManager->finalizeSDL();
-                windowManager->destroyWindow();
+            if(engine::InputManager::instance.getQuitRequest()) {
+                is_running = false;
+                sdl_manager->finalizeSDL();
+                window_manager->destroyWindow();
                 continue;
             }
 
 
-            timeElapsed = SDL_GetTicks() - stepTime;
+            time_elapsed = SDL_GetTicks() - step_time;
             DEBUG("TICKS:" + std::to_string(SDL_GetTicks()));
-            DEBUG("frameTime:" + std::to_string(frameTime));
-            DEBUG("timeElapsed: " + std::to_string(timeElapsed));
-            if(frameTime > timeElapsed){
-                DEBUG("SDL_DELAY: " + std::to_string(frameTime - timeElapsed));
-                SDL_Delay(frameTime - timeElapsed);
-                timeElapsed = SDL_GetTicks() - stepTime;
+            DEBUG("frameTime:" + std::to_string(frame_time));
+            DEBUG("timeElapsed: " + std::to_string(time_elapsed));
+            if(frame_time > time_elapsed) {
+                DEBUG("SDL_DELAY: " + std::to_string(frame_time - time_elapsed));
+                SDL_Delay(frame_time - time_elapsed);
+                time_elapsed = SDL_GetTicks() - step_time;
             }
 
-            if(sceneManager->getCurrentScene() != NULL){
-              sceneManager->getCurrentScene()->update(timeElapsed);
-              sceneManager->getCurrentScene()->draw();
+            if(scene_manager->get_current_scene() != NULL) {
+              scene_manager->get_current_scene()->update(time_elapsed);
+              scene_manager->get_current_scene()->draw();
             }
             AnimationManager::instance.draw_quads();
             SDL_RenderPresent(WindowManager::getGameCanvas());
--- game/src/tutorial_scene.cpp
+++ game/src/tutorial_scene.cpp
@@ -1,27 +1,27 @@
 #include "tutorial_scene.hpp"
 
-TutorialScene::TutorialScene(int id, std::string backgroundFile, int nextScene): Scene(id){
-    background = new Sprite(backgroundFile);
-    newNextScene = nextScene;
-    sceneTimer = new Timer();
+TutorialScene::TutorialScene(int id, std::string background_file, int next_scene): Scene(id) {
+    background = new Sprite(background_file);
+    new_next_scene = next_scene;
+    scene_timer = new Timer();
 }
 
-void TutorialScene::draw(){
+void TutorialScene::draw() {
     background->draw(0,0);
 }
 
-void TutorialScene::update(double timeElapsed){
-    timeElapsed = timeElapsed;
-    if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_ENTER) || sceneTimer->total_elapsed_time() >= 4000){
-        getSceneManager()->loadScene(newNextScene);
+void TutorialScene::update(double time_elapsed) {
+    time_elapsed = time_elapsed;
+    if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_ENTER) || scene_timer->total_elapsed_time() >= 4000) {
+        getSceneManager()->loadScene(new_next_scene);
     }
 }
 
-void TutorialScene::load(){
-    sceneTimer->start();
+void TutorialScene::load() {
+    scene_timer->start();
     gameObjectsList.push_back(std::pair<int, GameObject*>(2,new Button("assets/fonts/font.ttf", 350, 400, 500, 500, "Continue", 50)));
 }
 
-void TutorialScene::unload(){
+void TutorialScene::unload() {
 
 }
