470d0500e0cea3d7ae9c043ba674b9fe11d7681c
Emanuel <manusenac@hotmail.com>
2017-09-03 11:24:37 -0300

    changes names
    change names of variables

--- game/src/button.cpp
+++ game/src/button.cpp
@@ -1,9 +1,9 @@
 #include "button.hpp"
 
-Button::Button(std::string objectName, double positionX, double positionY,
+Button::Button(std::string objectName, double position_x, double position_y,
                                      int width, int height, std::string title, int sizeTitle) : GameObject(objectName,
-                                                                         positionX,
-                                                                         positionY,
+                                                                         position_x,
+                                                                         position_y,
                                                                          width, height){
 
     text = new Text(title, objectName, sizeTitle, true, new Color(0,0,0,0), new Color(0,0,0,0));
@@ -11,9 +11,9 @@ Button::Button(std::string objectName, double positionX, double positionY,
 
 Button::~Button(){}
 
-void Button::update(double timeElapsed){
-    timeElapsed = timeElapsed;
-    INFO(timeElapsed);
+void Button::update(double time_elapsed){
+    time_elapsed = time_elapsed;
+    INFO(time_elapsed);
 
 }
 
