728a525fecbeae2e5a687174964575fb81450cb8
Emanuel <manusenac@hotmail.com>
2017-09-03 11:23:08 -0300

    changes names
    change variables names from camel_case to snake_case.

--- game/src/arrow.cpp
+++ game/src/arrow.cpp
@@ -1,10 +1,10 @@
 #include "arrow.hpp"
 #include "log.h"
 
-Arrow::Arrow(std::string objectName, double positionX, double positionY,
+Arrow::Arrow(std::string objectName, double position_x, double position_y,
              int width, int height) : GameObject(objectName,
-                                                 positionX,
-                                                 positionY,
+                                                 position_x,
+                                                 position_y,
                                                  width, height){
         animator = new Animation(objectName, 1, 17, 2.0);
         animator->addAction("select",0,16);
@@ -15,8 +15,8 @@ Arrow::~Arrow(){
 
 }
 
-void Arrow::update(double timeElapsed){
-        timeElapsed = timeElapsed;
+void Arrow::update(double time_elapsed){
+        time_elapsed = time_elapsed;
         if(isEnabled()){
                 animator->update();
         }
