5f3f3cfc85ccb292b668c0f07021fa91a9a9b091
Emanuel <manusenac@hotmail.com>
2017-09-10 19:23:06 -0400

    comentarios da classe menu_scene.cpp
    comentarios das variaveis, metodos , paragrafos e estrutura de decisao

--- game/src/menu_scene.cpp
+++ game/src/menu_scene.cpp
@@ -5,21 +5,27 @@
 #include <iostream>
 
 using namespace engine;
-
+/** set image and sound without character
+*@param scene id.
+*@return SaveManager 
+*/ 
 MenuScene::MenuScene(int id) : Scene(id){
-        selectButton = 1;
-        select = new Color(255, 255, 255, 0);
-        notSelect = new Color(0, 0, 0, 0);
-        background = new Sprite("assets/sprites/background.png");
-        buttonEffect = new Audio("assets/sounds/SELECT6.wav", "EFFECT", 100);
-        selectEffect = new Audio("assets/sounds/SELECT5.wav", "EFFECT", 128);
-        backgroundMusic = new Audio("assets/sounds/MENU.wav", "MUSIC", 100);
+        selectButton = 1; /** select button*/
+        select = new Color(255, 255, 255, 0);/** select color if select */
+        notSelect = new Color(0, 0, 0, 0);/** select color if diselect*/
+        background = new Sprite("assets/sprites/background.png");/** photo background*/
+        buttonEffect = new Audio("assets/sounds/SELECT6.wav", "EFFECT", 100);/**  sound of button*/
+        selectEffect = new Audio("assets/sounds/SELECT5.wav", "EFFECT", 128);/** select sound*/
+        backgroundMusic = new Audio("assets/sounds/MENU.wav", "MUSIC", 100);/** background music*/
         SaveManager::instance.verifySave();
 }
 
 MenuScene::~MenuScene(){
 }
 
+/** draw the scene background 
+*@return gameObject the object game.
+*/
 void MenuScene::draw(){
         background->draw(0,0);
 
@@ -27,13 +33,16 @@ void MenuScene::draw(){
                 (*gameObject.second).draw();
         }
 }
-
+/** moves  for  change and select button
+*@param timeEleapsed  time which eleapsed.
+*@return  gameObject the object game.
+*/
 void MenuScene::update(double timeElapsed){
         selectAction();
 
-        for(auto gameObject : gameObjectsList) {
-                if(typeid(*gameObject.second) == typeid(Button)) {
-                        if(gameObject.first == selectButton) {
+        for(auto gameObject : gameObjectsList) {/**  action anthil don't exist objects*/
+                if(typeid(*gameObject.second) == typeid(Button)) {/** action if button is pressed*/
+                        if(gameObject.first == selectButton) {/** action selectButton is pressed in the positive change color text to select another wat to disselect.*/
                                 ((Button *)(gameObject.second))->setTextColor(select);
                         }else{
                                 ((Button *)(gameObject.second))->setTextColor(notSelect);
@@ -44,8 +53,9 @@ void MenuScene::update(double timeElapsed){
         }
 
 }
-
+/** capture the button pressed from the user , if it's a pause do which , move the pointer and load or quit th scene*/
 void MenuScene::selectAction(){
+        /**  if button up was pressed  would up the   select pointer and set button to tree*/ 
         if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_UP)) {
                 buttonEffect->pause();
                 buttonEffect->play(0);
@@ -54,6 +64,7 @@ void MenuScene::selectAction(){
                         selectButton = 3;
                 }
         }
+        /**  if button down was pressed  would down the   select pointer and set button to tree*/
         else if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_DOWN)) {
                 buttonEffect->pause();
                 buttonEffect->play(0);
@@ -62,7 +73,7 @@ void MenuScene::selectAction(){
                         selectButton = 1;
                 }
         }
-
+        /** change scene according to button pressed , if button pressed is   down close the game , if up at fist time laod the scene 22 , else if at the second  load the scene 8 and else  the down action*/         
         if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_ENTER)) {
                 selectEffect->play(0);
                 switch(selectButton) {
@@ -81,7 +92,9 @@ void MenuScene::selectAction(){
 
         }
 }
-
+/** game_object according to  button press following the  action made by the Select Action method
+ *@return gameObject  the game object.
+*/ 
 void MenuScene::load(){
         gameObjectsList.push_back(std::pair<int, GameObject*>(1, new Button("assets/fonts/font.ttf", 410, 300, 500, 500, "Jogar", 50)));
         gameObjectsList.push_back(std::pair<int, GameObject*>(2,new Button("assets/fonts/font.ttf", 290, 400, 500, 500, "Selecionar Fase", 50)));
@@ -90,5 +103,5 @@ void MenuScene::load(){
         AnimationManager::instance.setBackgroundColor(new Color(100,100,100, 125));
 }
 
-void MenuScene::unload(){
+void MenuScene::unload(){/** better organize of game  since there is a method to do nothing. */
 }
