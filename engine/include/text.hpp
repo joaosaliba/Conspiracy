b3298aa814af9f07ae9c3c6500db1ee4a5a173d1
Emanuel <manusenac@hotmail.com>
2017-09-10 19:04:20 -0400

    comentarios
    comentarios da classe text.hpp variaveis, rotinas, metodos e estrtura de decisão

--- engine/include/text.hpp
+++ engine/include/text.hpp
@@ -7,9 +7,11 @@
 #include "sdl2includes.hpp"
 #include "engine.hpp"
 #include "color.hpp"
-
+/** set text   aspects as zie, font, luanity
+ *@param newtext, font_path, new_size, ishigh quality, new_back_ground, new_text_color*/ 
+*/ 
 namespace engine{
-
+    /**  set the text 
     class Text{
     public:
         Text( std::string  newText, std::string newFontPath, int newSize, bool isHighQuality, Color* newBackground, Color* newTextColor);
@@ -17,24 +19,25 @@ namespace engine{
 
         void init();
         void draw(int x, int y);
-        void shutdown();
-        void loadFont();
-        void setBackground(Color * newBackground);
-        void setTextColor(Color * newTextColor);
+        void shutdown();/** delete the text from display*/
+        void loadFont();/** retrail the text font*/
+        void setBackground(Color * newBackground);/** set the text background*/
+        void setTextColor(Color * newTextColor);/** set the text color*/
     protected:
-        SDL_Texture * texture;
-        TTF_Font * font;
+        SDL_Texture * texture; /** the text texture */ 
+        TTF_Font * font;/** the text font*/
 
-        std::string text;
-        std::string fontPath;
-        int size;
-        bool highQuality;
+        std::string text; /** the text itself*/
+        std::string fontPath;/**the font self*/
+        int size;/** the text size*/
+        bool highQuality;/** the text quality*/
 
-        Color *background;
-        Color *textColor;
-        int width;
-        int height;
+        Color *background;/** the text background*/
+        Color *textColor;/** the text color*/
+        int width;/** the text width*/
+        int height;/** the text high*/
     };
 }
 
 #endif
+
