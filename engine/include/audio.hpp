2e50e0c5b7bceb9373afdc176596759f0bb2e42a
geovanne <geovannessaraiva97@gmail.com>
2017-09-11 03:06:48 -0300

    style sheet and header in audio.cpp and hpp

--- engine/include/audio.hpp
+++ engine/include/audio.hpp
@@ -1,3 +1,7 @@
+/**class to object of audio, where you can find some values, files, others objects
+ *and meths to the same.
+ */
+
 #ifndef AUDIO_HPP
 #define AUDIO_HPP
 
@@ -9,15 +13,15 @@ namespace engine{
 
     class Audio{
     public:
-        Audio(std::string audioPath, std::string audioType, int volume);
+        Audio(std::string audio_path, std::string audio_type, int volume);
         ~Audio();
-        void play(const int timesToPlay);
+        void play(const int times_to_play);
         void pause();
         void resume();
         void stop();
     private:
-        Mix_Music* audioMusic = NULL;
-        Mix_Chunk* audioEffect = NULL;
+        Mix_Music* audio_music = NULL;
+        Mix_Chunk* audio_effect = NULL;
     };
 }
 #endif
