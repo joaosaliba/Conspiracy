aaa5ff5d703eaa5b77266568a7888833564f4b1a
geovanne <geovannessaraiva97@gmail.com>
2017-09-11 23:52:37 -0300

    comment in timer.cpp and hpp

--- engine/src/timer.cpp
+++ engine/src/timer.cpp
@@ -6,23 +6,54 @@
 
 using namespace engine;
 
+/**
+* Método start
+* <p>Esse método executa a função de dar partida no game</p>
+*/
 void Timer::start() {
     ticks = step_ticks = SDL_GetTicks();
 }
+
+/**
+* Método stop
+* <p>Esse método executa a função parar o game</p>
+*@param unsigned int-ticks
+*@param unsigned int-step_ticks
+*/
 
 void Timer::stop() {
     ticks = 0;
     step_ticks = 0;
 }
 
+/**
+* Método step
+* <p></p>
+*@param unsigned int-ticks
+*@param unsigned int-step_ticks
+*/
 void Timer::step() {
     step_ticks = SDL_GetTicks();
 }
 
+/**
+* Método elapsed_time
+* <p>Esse método calcula o tempo gasto</p>
+*@param unsigned int-elapsed_time
+*@param unsigned int-step_ticks
+*@return elapsed_time
+*/
 unsigned int Timer::elapsed_time() {
     return SDL_GetTicks() - step_ticks;
 }
 
+/**
+* Método total_elapsed_time
+* <p>Esse método calcula o tempo total gastono jogo</p>
+*@param unsigned int-total_elapsed_time
+*@param unsigned int-step_ticks
+*@return total_elapsed_time
+*/
 unsigned int Timer::total_elapsed_time() {
     return SDL_GetTicks() - ticks;
 }
