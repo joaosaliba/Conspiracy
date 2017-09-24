961a3d3d795c54496ff2c4b5e44191c57d9b0c6c
Daniel Oda <danieloda35@gmail.com>
2017-09-24 20:59:14 -0300

    Adding assertives and significative names

--- engine/include/rectangle.hpp
+++ engine/include/rectangle.hpp
@@ -11,18 +11,18 @@ using namespace engine;
 
 class Rectangle: public GameObject{
 public:
-    Rectangle(std::string object_name, double position_x, double position_y, int width, int height, bool new_alpha_active, Color * newColor);
+    Rectangle(std::string object_name, double rectangle_position_x, double rectangle_position_y, int rectangle_width, int rectangle_height, bool new_alpha_active, Color * rectangle_new_color);
     ~Rectangle();
     void update(double time_elapsed);
     void draw();
     void init();
     void setAlphaActive(bool is_active);
     bool getAlphaActive();
-    void setColor(Color * newColor);
+    void setColor(Color * rectangle_new_color);
 private:
     bool alpha_active;
     SDL_Texture* texture;
-    Color* color;
+    Color* rectangle_color;
 
     void verifyAlpha();
 };
--- engine/src/rectangle.cpp
+++ engine/src/rectangle.cpp
@@ -1,13 +1,16 @@
+//#define NDEBUG *uncomment to disable assertions
+#include <assert.h>
 #include "rectangle.hpp"
 
-Rectangle::Rectangle(std::string object_name, double position_x, double position_y,
-  int width, int height, bool new_alpha_active, Color * newColor): GameObject(object_name,
-                                                 position_x,
-                                                 position_y,
-                                                 width, height){
+Rectangle::Rectangle(std::string object_name, double rectangle_position_x, double rectangle_position_y,
+  int rectangle_width, int rectangle_height, bool new_alpha_active, Color * rectangle_new_color): GameObject(object_name,
+                                                 rectangle_position_x,
+                                                 rectangle_position_y,
+                                                 rectangle_width, rectangle_height){
 
-      color = newColor;
+      rectangle_color = rectangle_new_color;
       alpha_active = new_alpha_active;
+      assert(rectangle_color != NULL);
       init();
 }
 
@@ -17,24 +20,23 @@ Rectangle::~Rectangle() {
 
 void Rectangle::init() {
     SDL_Surface *surface;
-
     surface = SDL_CreateRGBSurface(0, getWidth(),getHeight(), 8, 0, 0, 0,0);
 
     if (surface == NULL) {
         ERROR("Create surface failed.");
         exit(-1);
     }
-
+    assert(surface != NULL);
     setWidth((int)(surface->w));
     setHeight((int)(surface->h));
 
     texture = SDL_CreateTextureFromSurface(WindowManager::getGameCanvas(), surface);
+    assert(texture != NULL);
     SDL_FreeSurface(surface);
 }
 
 void Rectangle::update(double time_elapsed){
     time_elapsed = time_elapsed;
-
     if(isEnabled()) {
         verifyAlpha();
     }
@@ -44,15 +46,14 @@ void Rectangle::draw() {
     if(isEnabled()) {
       SDL_Rect clipRect = {0, 0, getWidth(), getHeight()};
       SDL_Rect renderQuad = {(int)getPositionX(), (int)getPositionY(), clipRect.w, clipRect.h};
-
       SDL_RenderCopy(WindowManager::getGameCanvas(), texture, &clipRect, &renderQuad);
     }
 }
 
 void Rectangle::verifyAlpha() {
     if(alpha_active){
-        SDL_SetTextureColorMod(texture, color->r, color->g, color->b);
-        SDL_SetTextureAlphaMod(texture, color->a);
+        SDL_SetTextureColorMod(texture, rectangle_color->r, rectangle_color->g, rectangle_color->b);
+        SDL_SetTextureAlphaMod(texture, rectangle_color->a);
         SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
     }
 }
@@ -65,6 +66,7 @@ bool Rectangle::getAlphaActive() {
     return alpha_active;
 }
 
-void Rectangle::setColor(Color * newColor){
-    color = newColor;
+void Rectangle::setColor(Color * rectangle_new_color){
+    assert(rectangle_new_color != NULL);
+    rectangle_color = rectangle_new_color;
 }
