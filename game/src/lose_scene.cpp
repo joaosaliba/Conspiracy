39784b7c837fdfad2940d96a446df5225b252cf9
Emanuel <manusenac@hotmail.com>
2017-09-25 21:03:10 -0400

    nomes significativos e assert

--- game/src/lose_scene.cpp
+++ game/src/lose_scene.cpp
@@ -1,77 +1,52 @@
-#include "log.h"
-#include "player.hpp"
+#include "lose_scene.hpp"
+#include "button.hpp"
+
+#include <typeinfo>
+#include <iostream>
+#include <assert.h>
 using namespace engine;
-# include<assert.h>
-Player::Player(std::pair<int, int> biluPosition, std::pair<int, int> etemerPosition,
-               std::pair<int,int> varginhaPosition, int paperQuantity, int stageNumber){
 
-        exclamationAnimation = new Animation("assets/sprites/exclamation2.png",1, 1, 0.5);
-
-        bilu = new Bilu(biluPosition.first, biluPosition.second);
-        varginha = new Varginha(varginhaPosition.first, varginhaPosition.second);
-        etemer = new Etemer(etemerPosition.first, etemerPosition.second);
-        header = new Header(0,0, paperQuantity, stageNumber);
-
-        selectedAlien = 1;
-
-        etemer->update(0);
-        bilu->update(0);
-        varginha->update(0);
-        header->update(0);
-
-        bilu->setAlienDeselect();
-        varginha->setAlienDeselect();
-        etemer->setAlienSelected();
-
-        bilu_sound_effect = new Audio("assets/sounds/TROCABILU.wav", "EFFECT", 100);
-        varginha_sound_effect = new Audio("assets/sounds/TROCAVARGINHA.wav", "EFFECT", 25);
-        etemer_sound_effect = new Audio("assets/sounds/TROCATEMER.wav", "EFFECT", 75);
+LoseScene::LoseScene(int id) : Scene(id){
+    selectButton = 1;
+    select = new Color(255, 255, 255, 0);
+    notSelect = new Color(0, 0, 0, 0);
+    soundEffect = new Audio("assets/sounds/SELECT6.wav", "EFFECT", 100);
+    backgroundMusic = new Audio("assets/sounds/GAMEOVER.wav", "MUSIC", 50);
+    background = new Animation("assets/sprites/lose.png", 1, 4, 0.8);
+    background->addAction("lose", 0,3);
+    background->setInterval("lose");
 }
 
-Player::~Player(){
+LoseScene::~LoseScene(){
 }
 
-void Player::update(double time_elapsed){
-        assert(time_elapsed>0);
-        int beforeAlien = selectedAlien;
+void LoseScene::draw(){
+        background->draw_instant(80, 20);
 
-        if(InputManager::instance.isKeyPressed(InputManager::KeyPress::KEY_PRESS_ONE)) {
-                selectedAlien = 1;
-        } else if(InputManager::instance.isKeyPressed(InputManager::KeyPress::KEY_PRESS_TWO)) {
-                selectedAlien = 2;
-        } else if(InputManager::instance.isKeyPressed(InputManager::KeyPress::KEY_PRESS_THREE)) {
-                selectedAlien = 3;
-                ((Varginha *)(varginha))->setDefault();
+        for(auto gameObject : gameObjectsList) {
+                (*gameObject.second).draw();
         }
+}
 
-        if(beforeAlien != selectedAlien) {
-                header->setAlienSelect(selectedAlien);
-                etemer->setAlienDeselect();
-                bilu->setAlienDeselect();
-                varginha->setAlienDeselect();
+void LoseScene::update(double timeElapsed){
+        assert(timeElapsed>0);
+        selectAction();
 
-                waitAnimation(beforeAlien);
-                switch(selectedAlien) {
-                case 1: etemer->setAlienSelected(); etemer_sound_effect->play(0); break;
-                case 2: bilu->setAlienSelected(); bilu_sound_effect->play(0); break;
-                case 3: varginha->setAlienSelected(); varginha_sound_effect->play(0);break;
+        background->update();
+
+        for(auto gameObject : gameObjectsList) {
+                if(typeid(*gameObject.second) == typeid(Button)) {
+                        if(gameObject.first == selectButton) {
+                                ((Button *)(gameObject.second))->setTextColor(select);
+                        }else{
+                                ((Button *)(gameObject.second))->setTextColor(notSelect);
+                        }
                 }
+
+                (*gameObject.second).update(timeElapsed);
         }
-
-        exclamationAnimation->update();
-
-        etemer->update(time_elapsed);
-        bilu->update(time_elapsed);
-        varginha->update(time_elapsed);
-        header->update(time_elapsed);
 }
 
-void Player::draw(){
-        INFO("PLAYER DRAW");
-        bilu->draw();
-        varginha->draw();
-        etemer->draw();
-        header->draw();
-}
-
-void Player::updatePaperQuantity(int newValue){
+void LoseScene::load(){
+        gameObjectsList.push_back(std::pair<int, GameObject*>(1,new Button("assets/fonts/font.ttf", 700, 500, 500, 500, "Continue", 50)));
+        gameObjectsList.push_back(std::pair<int, GameObject*>(2,new Button("assets/fonts/font.ttf", 50, 500, 500, 500, "Menu", 50)));
