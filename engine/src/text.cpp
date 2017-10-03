8b3dd4c620521a4c30aa01683e67e608a95cd61f
Emanuel <manusenac@hotmail.com>
2017-10-02 21:12:53 -0400

    comportamento default e verificar retorno

--- engine/src/text.cpp
+++ engine/src/text.cpp
@@ -1,98 +1,128 @@
-#include "text.hpp"
-#include "log.h"
-#include "engine.hpp"
-#include "text_manager.hpp"
+#include "paper.hpp"
+#include <assert.h>
+Paper::Paper(std::string objectName, double positionX, double positionY,
+                                     int width, int height) : GameObject(objectName,
+                                                                         positionX,
+                                                                         positionY,
+                                                                         width, height){
 
-using namespace engine;
+    animator = new Animation(objectName, 1, 4, 0.5);
+    assert(animator->matrix.first.rows==1);
 
-Text::Text( std::string  newText, std::string new_font_path, int new_size, bool is_high_quality, Color *new_back_ground, Color *new_text_color){
-    text = new_text;/** text itself*/
-    fontPath = new_font_path;/** fontPath font path*/
-    size = new_size;/** size of text */
-    font = NULL;/** font to the  text*/ 
-    high_1uality = is_high_quality;/** high_1uality quality level of the text*/
-    background = new_background;/** background form the text*/
-    text_color = new_textColor;/** text_color color which text we display*/
-    init();
+    std::vector<unsigned int> backColor = {22, 206, 26, 1};
+    std::vector<unsigned int> frontColor = {116, 225, 117, 1};
+    editing_bar = new ProgressBar(positionX-3, positionY-8, 22.5, 5, 0.005, backColor, frontColor);
+    assert(editing_bar-> back_rect->position_x==positionX-3);
+    paperEditingSound = new Audio("assets/sounds/PAPEROISE.wav", "EFFECT", 100);
+    
+    animator->addAction("idle",0,0);
+    assert(std::get<animator->lis_actions[idle][1]>==0);
+    animator->addAction("beingEdited",1,3);
+    assert(std::get<animator->lis_actions[beignEdited][1]>==1);
+    edited = false;
+    isBeingEdited = false;
 }
 
+Paper::~Paper(){}
 
-void Text::init(){
-
-    if (fontPath == ""){/** fontPath font path,  exit if  which  doen't exist*/ 
+void Paper::update(double timeElapsed){
+    timeElapsed = timeElapsed;
+    if(isBeingEdited){
+        editing_bar->update(timeElapsed);
+        asert(editing_bar.getPositionX()==positionX-3);// verificar se editing bar foi editado. 
+        animator->setInterval("beingEdited");
+        if(editing_bar->getPercent() <= 0.0){
+            isBeingEdited = false;
+            edited = true;
+        }else{
+        
+        
+        
+        
+    if (font_path == "") {
         WARN("Invalid path for font!");
         exit(-1);
-    }
+    }else{ 
+// do nothefing.
 
-    font = TextManager::instance.loadFont(fontPath, size);
+    font = TextManager::instance.loadFont(font_path, size);
 
-    SDL_Color color = {textColor->r, textColor->g, textColor->b, textColor->a};
+    SDL_Color color = {text_color->r, text_color->g, text_color->b, text_color->a};
     SDL_Color bg_color = {background->r, background->g, background->b, background->a};
 
-    SDLSurface * surface = NULL;
-    /** if the text is in high quality and align render another else move to next clause */ 
-    if (highQuality && bg_color.a == 0x00){ /** render the text since is in high quality and transparent */ 
+    SDL_Surface * surface = NULL;
+
+    if (high_quality && bg_color.a == 0x00) {
         surface = TTF_RenderText_Blended(
             font, text.c_str(), color
         );
     }
-    else if (high_quality){/** render the text since it's in high quality and isn't transparent   or move to another clause*/
+    else if (high_quality) {
         surface = TTF_RenderText_Shaded(
             font, text.c_str(), color, bg_color
         );
     }
-    else{/** render the text solid since another clauses is false*/
+    else {
         surface = TTF_RenderText_Solid(
             font, text.c_str(), color
         );
     }
 
-    if(surface == NULL){/** if the text doen't have surface  exit the program*/
+    if(surface == NULL) {
         exit(-1);
-    }
+    }else{ 
+//do nothefing
 
-    if(texture != NULL){ /**  destroy the texture to text since present or move to another clause*/
-		SDL_DestroyTexture(texture);
-	}
+    if(texture != NULL) {
+SDL_DestroyTexture(texture);
+}else{
+// do nothefing.
 
     texture = SDL_CreateTextureFromSurface(WindowManager::getGameCanvas(), surface);
 
-    if (texture == NULL){ /** doesn't destroy since it's not needed*/
+    if (texture == NULL) {
         exit(-1);
-    }
+    }else{
+//do nothefing.
 
     width = surface->w;
     height = surface->h;
 
-    SDL_FreeSurface(surface);
+    }else{
+        animator->setInterval("idle");
+    }
+    animator->update();
 }
 
-void Text::shutdown(){
-    INFO("Shutdown Text");
-
-    SDL_DestroyTexture(texture);
-    texture = NULL; /** text texture*/
-
-    font = NULL; /** font to the  text*/ 
-
+void Paper::animate(){
+    isBeingEdited = true;
 }
 
-void Text::draw(int x, int y){
-	 /** x text position in x axis, y position in y axis,  width and heigt to text which this names repesctively 
-	 *width and heigh*/ 
-    SDL_Rect render_quad = { x,y,width, heigh
-    };
-
-    SDL_RenderCopy(WindowManager::getGameCanvas(), texture, NULL, &render_quad);
+void Paper::stopAnimation(){
+    isBeingEdited = false;
 }
 
-
-void Text::setBackground(Color * newBackground){
-    background = newBackground;/** background text background*/
-    init();
+bool Paper::isEdited(){
+    return edited;
 }
 
-void Text::setTextColor(Color * newTextColor){
-    textColor = newTextColor;/** textcolor text color*/
-    init();
+void Paper::draw(){
+    INFO("Paper DRAW");
+    asert(animator->getPositionX()>0);
+    assert(animatior->getPositionY()>0);
+    assert(animator->getWidth()>0);
+    assert(animator->getHeight()>0);
+    animator->draw(getPositionX(), getPositionY());
+    animator->draw_collider(getPositionX(), getPositionY(), getWidth(), getHeight());
+    if(isBeingEdited){
+        AnimationManager::instance.addProgressBar(editing_bar);
+    }
+}
+
+Animation * Paper::getAnimation(){
+  return animator;
+}
+
+double Paper::getEditingBarPercent(){
+    return editing_bar->getPercent();
 }
