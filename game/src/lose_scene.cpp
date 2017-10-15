5769f2a5d81cc555c00373f3f06eef99d55cc575
Emanuel <manusenac@hotmail.com>
2017-10-15 20:40:11 -0200

    magic numbers and atomic functions

--- game/src/lose_scene.cpp
+++ game/src/lose_scene.cpp
@@ -1,19 +1,89 @@
 #include "lose_scene.hpp"
 #include "button.hpp"
+#define int  NOTSELECTEDCOLOR* ;
+#define int  SELECTEDCOLOR*;
+#define cons int  SOUNDEFFECT=100;
+#define const int BACKGROUNMUSIC=50;
+#define type def struct CORDENATES{
+  Int X;
+ Int Y;
+}
+#define type def struct POSITION{
+  Int BEGIN;
+ Int LAST;
+}
+POSITION.BEGIN= 0;
+POSITION.LAST=3;
+# define  type def struct SIZE{
+   ROWS
+  COLUMNS
+}
+#define type def struct SIZEOBJECT{
+    int WiTDTH;
+    int HEIGH ;
+}
+#define const  COORDENATES ANIMATIONPOSITION.X=80;
+#define const   COORDENATES ANIMATIONPOSITION.Y=20;
+#define const  SIZE ANIMATION
+#define const SIZEOBJECT OBJECTCONTINUE;
+#define const OBJECTCONTINUE.WIDTH=700;
+#define const OBJECTCONTINUE.HEIGH=500;
+#define ANIMATION.COLUMS=4;
+#define const ANIMATION.ROWS=1;
+#define POSITION OBJECTCONTINUE;
+#define int
+#int CONST  POSITIONCONTINUE.BEGIN=500;
+#define int CONST  POSITIONCONTINUE.LAST=500;
+#define const int NUMBERCONTINUE =1;
+#define const int =2;
+#define POSITION POSITIONMENU;
+ #define POSITIONMENU.BEGIN= 500;
+#define  POSITIONMENU.LAST=500;
+# const SIZEOBJECT OBJECTMENU.WIDTH= 50;
+#const SIZEOBJECT OBJECTMENU.HEIGH=500;
+#define int const TIMESTOPLAY =0;
+#define int  BUTTONCONITNUEWITHOUTDIRECTIONAL=1;
+#define int const BUTTONMENUWITHOUTDIRECTIONAL=2;
 
+ Struct type def int back_ground_color{
+  int red;
+ int green ; 
+ int black;
+ int alpha;
+} 
+#define  const  back_ground_color BACKGROUND.red= 255; 
+#define const  back_ground_color  BACKGROUND.green=160;
+#define    const  back_ground_color  BACKGROUND.black=156;
+#define    const  back_ground_color  BACKGROUND.alpha=125;
+ # define const int NUMBERLOWERTHANCONTINUE=0;
+#define const int NUMBERMORETHANMENU=2;
+,
+
+.ROWS= 1;
+ANIMATIONSIZE.COLUMNS=4;
+#define double  ANINMATIONTIME= 0.8;
+Const SELECTEDCOLOR[0]=255;
+Const SELECTEDCOLOR[1]=255;
+Const SELECTEDCOLOR[2]=255;
+Const SELECTEDCOLOR[3]=0;
+
+SELECTEDCOLOR
+NOTSELECTEDCOLOR
+ 
 #include <typeinfo>
 #include <iostream>
-#include <assert.h>
+
 using namespace engine;
 
 LoseScene::LoseScene(int id) : Scene(id){
     selectButton = 1;
-    select = new Color(255, 255, 255, 0);
-    notSelect = new Color(0, 0, 0, 0);
-    soundEffect = new Audio("assets/sounds/SELECT6.wav", "EFFECT", 100);
-    backgroundMusic = new Audio("assets/sounds/GAMEOVER.wav", "MUSIC", 50);
-    background = new Animation("assets/sprites/lose.png", 1, 4, 0.8);
-    background->addAction("lose", 0,3);
+    select = new Color(SELECTEDCOLOR[0], SELECTEDCOLOR[1], SELECTEDCOLOR[2], SELECTEDCOLOR[3]);
+    notSelect = new Color(NOTSELECTEDCOLOR[0], NOTSELECTEDCOLOR[1], NOTSELECTEDCOLOR[2], NOTSELECTEDCOLOR[3]);
+
+    soundEffect = new Audio("assets/sounds/SELECT6.wav",”EFFECT”, SOUNDEFFECT );
+    backgroundMusic = new Audio("assets/sounds/GAMEOVER.wav", "MUSIC", BACKGROUNMUSIC );
+    background = new Animation("assets/sprites/lose.png",ANIMATIONSIZE.X, ANIMATIONSIZE.Y,ANIMATIONTIME);
+    background->addAction("lose",POSITION.BEGIN,POSITION.LAST);
     background->setInterval("lose");
 }
 
@@ -21,7 +91,7 @@ LoseScene::~LoseScene(){
 }
 
 void LoseScene::draw(){
-        background->draw_instant(80, 20);
+        background->draw_instant(ANIMATIONPOSITION.X , ANIMATIONPOSITION.Y);
 
         for(auto gameObject : gameObjectsList) {
                 (*gameObject.second).draw();
@@ -29,7 +99,6 @@ void LoseScene::draw(){
 }
 
 void LoseScene::update(double timeElapsed){
-        assert(timeElapsed>0);
         selectAction();
 
         background->update();
@@ -48,5 +117,55 @@ void LoseScene::update(double timeElapsed){
 }
 
 void LoseScene::load(){
-        gameObjectsList.push_back(std::pair<int, GameObject*>(1,new Button("assets/fonts/font.ttf", 700, 500, 500, 500, "Continue", 50)));
-        gameObjectsList.push_back(std::pair<int, GameObject*>(2,new Button("assets/fonts/font.ttf", 50, 500, 500, 500, "Menu", 50)));
+        gameObjectsList.push_back(std::pair<int, GameObject*>( NUMBERCONTINUE,new Button("assets/fonts/font.ttf", OBJECTCONTINUE.WIDTH, OBJECTCONTINUE.HEIGH
+POSITIONCONTINUE.BEGIN, POSITIONCONTINUE.LAST, "Continue", 50)));
+        gameObjectsList.push_back(std::pair<int, GameObject*>( NUMBERMENU,new Button("assets/fonts/font.ttf" OBJECTMENU.WIDTH, OBJECTMENU.HEIGH, POSITIONMENU.BEGIN , POSITIONMENU.LAST,"Menu", 50)));
+        AnimationManager::instance.setBackgroundColor(new Color(BACKGROUND.red, BACKGROUND.green, BACKGROUND.black, BACKGROUND.alpha));
+));
+        backgroundMusic->play(0);
+
+}
+void  selectActionLessThanContinue(){
+                selectButton--;
+                if(selectButton <= NUMBERLOWERTHANCONTINUE ) {
+                        selectButton = NUMBERMENU;
+                }
+}    
+ void selectActionMoreThanMenu() {
+                selectButton++;
+             if(selectButton > NUMBERMORETHANMENU) {
+                        selectButton = NUMBERCONTINUE;
+                }
+} 
+ void selectActionEnter();
+                switch(selectButton) {
+                case SELECTBUTTONCONITNUEWITHOUTDIRECTIONAL:
+                        getSceneManager()->loadScene(getSceneManager()->get_before_scene_id());
+                        break;
+                case BUTTONMENUWITHOUTDIRECTIONAL:
+                        getSceneManager()->loadScene(0);
+                        break;
+                default:
+                        break;
+                }
+}
+
+void  selectActionLeftorRightButton(){ 
+               soundEffect->pause();
+                soundEffect->play(TIMESTOPLAY);  
+       if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_LEFT)){
+    selectActionLessThanContinue();
+}else{     if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_RIGHT) {
+         selectActionMoreThanMenu();
+}
+
+void LoseScene::selectAction(){
+if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_ENTER)){
+  selectActionEnter()
+}else{
+ selectActionLeftorRightButton()
+}
+ 
+
+void LoseScene::unload(){
+}
