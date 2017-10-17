6d99ecc730956c42d95777272368904e16aa3f07
Emanuel <manusenac@hotmail.com>
2017-10-16 22:58:30 -0200

    magic numbers and small functions

--- game/src/paper.cpp
+++ game/src/paper.cpp
@@ -1,79 +1,141 @@
+//#define NDEBUG *uncomment to disable assertions
 #include "paper.hpp"
+#include <assert.h>
+struct color {
+  int red;
+  int green;
+  int black;
 
-Paper::Paper(std::string objectName, double positionX, double positionY,
-                                     int width, int height) : GameObject(objectName,
-                                                                         positionX,
-                                                                         positionY,
-                                                                         width, height){
+  double alpha; 
+}
+BACKCOLOR.red,  BACKCOLOR.green, BACKCOLOR.black,BACKCOLOR.alpha
+#define  const color BACKCOLOR.red=22
+#define const color  BACKCOLOR.green=206;
+#define const color BACKCOLOR.black=26;
+#define const color BACKCOLOR.alpha=1;
+#define const color FRONTCOLOR.red=116;
+#define const color FRONTCOLOR.green=225;
+#define const color FRONTCOLOR.black=117;
+#define const color FRONTCOLOR.alpha=0.5;
+struct  size{
+   int rows;
+   int columns;
+}
+struct size_figure{
+  int with;
+  int heigh;
+}
 
-    animator = new Animation(objectName, 1, 4, 0.5);
+struct margin{
+   int left;
+   int heigh;
+}
 
-    std::vector<unsigned int> backColor = {22, 206, 26, 1};
-    std::vector<unsigned int> frontColor = {116, 225, 117, 1};
-    editing_bar = new ProgressBar(positionX-3, positionY-8, 22.5, 5, 0.005, backColor, frontColor);
+MARGIN.left,MARGIN.heigh
+#define const  margin MARGIN.left=3
+#define const  margin MARGIN.heigh=8; 
+#define  const size ANIMATION.rows=1;
+#define const size ANIMATION.columns=4;
+# define  const int TIME=0.5;
+#define const size_figure PROGRESS.width=22.5;
+#define const size_figure PROGRESS.heigh=5;
+#define const int INCREMENT=0.005;
+# const int TIMESTOPLAY =0
+Paper::Paper(std::string object_name, double paper_position_x, double paper_position_y,
+                                     int paper_width, int paper_height) : GameObject(object_name,
+                                                                         paper_position_x,
+                                                                         paper_position_y,
+                                                                         paper_width, paper_height) {
 
-    paperEditingSound = new Audio("assets/sounds/PAPEROISE.wav", "EFFECT", 100);
+    animator = new Animation(object_name, ANIMATION.rows,ANIMATION.columns,TIME);
+
+    std::vector<unsigned int> back_color = {BACKCOLOR.red,  BACKCOLOR.green, BACKCOLOR.black,BACKCOLOR.alpha};
+    std::vector<unsigned int> front_color = {FRONTCOLOR.red,FRONTCOLOR.green,FRONTCOLOR.black,FRONTCOLOR.alpha};
+    paper_editing_bar = new ProgressBar(paper_position_x-MARGIN.left, paper_position_y-MARGIN.heigh,PROGRESS.width,PROGRESS.heigh, INCREMENT, back_color, front_color);
+
+    paper_editing_sound = new Audio("assets/sounds/PAPEROISE.wav", "EFFECT", 100);
 
     animator->addAction("idle",0,0);
     animator->addAction("beingEdited",1,3);
-    edited = false;
-    isBeingEdited = false;
+    paper_edited = false;
+    paper_is_being_edited = false;
+    assert(paper_edited != NULL);
+    assert(paper_is_being_edited != NULL);
 }
 
 Paper::~Paper(){}
 
-void Paper::update(double timeElapsed){
-    timeElapsed = timeElapsed;
-    if(isBeingEdited){
-        editing_bar->update(timeElapsed);
+void Paper::update(double time_elapsed){
+    time_elapsed = time_elapsed;
+    //If paper is being edited it sets the paper editing bar
+    if(paper_is_being_edited) {
+        paper_editing_bar->update(time_elapsed);
         animator->setInterval("beingEdited");
-        if(editing_bar->getPercent() <= 0.0){
-            isBeingEdited = false;
-            edited = true;
+        //If paper is being edited it gets the percentage of the edition
+        if(paper_editing_bar->getPercent() <= 0.0){
+            paper_is_being_edited = false;
+            paper_edited = true;
+            assert(paper_edited != NULL);
+            assert(paper_is_being_edited != NULL);
+        //Paper is not being edited
+        }else {
+            animator->setInterval("idle");
+            animator->update();
         }
-    }else{
-        animator->setInterval("idle");
+
+    }else {
+      //nothing to do
     }
-    animator->update();
 }
 
 void Paper::animate(){
-    isBeingEdited = true;
+    paper_is_being_edited = true;
+    assert(paper_is_being_edited != NULL);
 }
 
 void Paper::stopAnimation(){
-    isBeingEdited = false;
+    paper_is_being_edited = false;
+    assert(paper_is_being_edited != NULL);
 }
 
 bool Paper::isEdited(){
-    return edited;
+    return paper_edited;
+    assert(paper_edited != NULL);
 }
 
 void Paper::draw(){
     INFO("Paper DRAW");
     animator->draw(getPositionX(), getPositionY());
     animator->draw_collider(getPositionX(), getPositionY(), getWidth(), getHeight());
-    if(isBeingEdited){
-        AnimationManager::instance.addProgressBar(editing_bar);
+    //It draws editing bar if the paper is being edited
+    if(paper_is_being_edited){
+        AnimationManager::instance.addProgressBar(paper_editing_bar);
+    }else {
+      //ProgressBar only apears if paper is being edited
     }
 }
 
 Animation * Paper::getAnimation(){
   return animator;
+  assert(animator != NULL);
 }
 
 double Paper::getEditingBarPercent(){
-    return editing_bar->getPercent();
+    return paper_editing_bar->getPercent();
+    assert(paper_editing_bar != NULL);
 }
 
 void Paper::resetEditingProgress(){
-    editing_bar->resetPercent();
+    paper_editing_bar->resetPercent();
+    assert(paper_editing_bar != NULL);
 }
 
 void Paper::playEffect(){
-    paperEditingSound->play(0);
+    paper_editing_sound->play(TIMESTOPLAY);
+    assert(paper_editing_sound != NULL);
 }
 
 void Paper::stopEffect(){
-    paperEditingSound->stop();
+    paper_editing_sound->stop();
+    assert(paper_editing_sound != NULL);
 }
