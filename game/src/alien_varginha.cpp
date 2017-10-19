9ec6c397664d6b4a8c68604bd9f01a381a3724c1
renatovalerio25@gmail.com <renatovalerio25@gmail.com>
2017-10-18 22:32:52 -0200

    Adding constants in alien_varginha.cpp

--- game/src/alien_varginha.cpp
+++ game/src/alien_varginha.cpp
@@ -5,6 +5,36 @@
 #define FILENAME "assets/sprites/varginha_sheet.png"
 #define WIDTH 10
 #define HEIGHT 15
+#define SCREEN_WIDTH 1000
+#define SCREEN_INITIAL 0
+#define SPECIAL_LEFT "special_left"
+#define SPRITE_INITIAL_SPECIAL_LEFT 10
+#define SPRITE_FINAL_SPECIAL_LEFT 11
+#define SPECIAL_RIGHT "special_right"
+#define SPRITE_INITIAL_SPECIAL_RIGHT 12
+#define SPRITE_FINAL_SPECIAL_RIGHT 13
+#define INVISIBLE_SPECIAL_LEFT "invisible_left"
+#define SPRITE_INITIAL_INVISIBLE_LEFT 11
+#define SPRITE_FINAL_INVISIBLE_LEFT 11
+#define INVISIBLE_SPECIAL_RIGHT "invisible_right"
+#define SPRITE_INITIAL_INVISIBLE_RIGHT 13
+#define SPRITE_FINAL_INVISIBLE_RIGHT 13
+#define ACTION_SPECIAL_LEFT "action_left"
+#define ACTION_INITIAL_LEFT 14
+#define ACTION_FINAL_LEFT 16 
+#define ACTION_SPECIAL_RIGHT "action_right"
+#define ACTION_INITIAL_RIGHT 17
+#define ACTION_FINAL_RIGHT 19 
+#define TOTAL_TIME_1 0.3
+#define VELOCITY_VARGINHA 0.15
+#define ACTION_IDLE_LEFT "idle_left"
+#define ACTION_IDLE_RIGHT "idle_right"
+#define NAME_CHARACTER "V"
+#define ANIMATION_NUMBER_1 5
+#define ANIMATION_NUMBER_2 0
+#define CENTER_CHARACTER_1 15
+#define CENTER_CHARACTER_2 25
+
 
 /** Objects included in the alien_varginha.hpp 
 * where the methods will be responsible for the manipulation and organization 
@@ -35,40 +65,28 @@ Alien(FILENAME, varginha_position_x, varginha_position_y, WIDTH, HEIGHT) {
     assert (HEIGHT > 15);
 
     // Verifies the position x of the character Varginha
-    if(varginha_position_x > 1000 || varginha_position_x < 0) {
+    if(varginha_position_x > SCREEN_WIDTH || varginha_position_x < SCREEN_INITIAL) {
         ERROR("Strange Varginha position x");
         exit(-1);
     }else {
        //nothing to do
     }
     // Verifies the position y of the character Varginha
-    if(varginha_position_y > 1000 || varginha_position_y < 0) {
+    if(varginha_position_y > SCREEN_WIDTH || varginha_position_y < SCREEN_INITIAL) {
        ERROR("Strange Varginha position y");
        exit(-1);
      }else {
        //nothing to do
      }
-    // Verifies the width of the character Varginha
-    if(WIDTH > 10 || WIDTH < 0) {
-       ERROR("Strange width");
-       exit(-1);
-     }else {
-       //nothing to do
-     }
-    // Verifies the height of the character Varginha 
-    if(HEIGHT > 15 || HEIGHT < 0) {
-       ERROR("Strange height");
-       exit(-1);
-     }else {
-       //nothing to do
-     }
 
-    animator->add_action("special_right",12,13);
-    animator->add_action("special_left",10,11);
-    animator->add_action("invisible_right", 13, 13);
-    animator->add_action("invisible_left", 11, 11);
-    animator->add_action("action_right", 17, 19);
-    animator->add_action("action_left", 14, 16);
+    animator->add_action(SPECIAL_RIGHT,SPRITE_INITIAL_SPECIAL_RIGHT,SPRITE_FINAL_SPECIAL_RIGHT);
+    animator->add_action(SPECIAL_LEFT,SPRITE_INITIAL_SPECIAL_LEFT,SPRITE_FINAL_SPECIAL_LEFT);
+    animator->add_action(INVISIBLE_SPECIAL_RIGHT,SPRITE_INITIAL_INVISIBLE_RIGHT,
+    SPRITE_FINAL_INVISIBLE_RIGHT);
+    animator->add_action(INVISIBLE_SPECIAL_LEFT,SPRITE_INITIAL_INVISIBLE_LEFT,
+    SPRITE_FINAL_INVISIBLE_LEFT);
+    animator->add_action(ACTION_SPECIAL_RIGHT,ACTION_INITIAL_RIGHT,ACTION_FINAL_RIGHT);
+    animator->add_action(ACTION_SPECIAL_LEFT,ACTION_INITIAL_LEFT,ACTION_FINAL_LEFT);
     is_invisible = false;
     is_selected = false;
     in_position = false;
@@ -105,9 +123,9 @@ void Varginha::update(double time_elapsed) {
        //nothing to do
      }
     in_position = false;
-    animator->set_total_time(0.3);
-    auto varginha_in_y = 0.15*time_elapsed;
-    auto varginha_in_x = 0.15*time_elapsed;
+    animator->set_total_time(TOTAL_TIME_1);
+    auto varginha_in_y = VELOCITY_VARGINHA*time_elapsed;
+    auto varginha_in_x = VELOCITY_VARGINHA*time_elapsed;
 
     //checks conditions to move the player Varginha 
     if(!block_movement && is_selected) {
@@ -122,10 +140,10 @@ void Varginha::update(double time_elapsed) {
         
         // If idle_animation_number true, receives actions
         if(idle_animation_number) {
-            animator->set_interval("idle_right");
+            animator->set_interval(ACTION_IDLE_RIGHT);
         //Assigns a default, and animator receives actions
         }else { 
-            animator->set_interval("idle_left");
+            animator->set_interval(ACTION_IDLE_LEFT);
         }
     }
     special_action();
@@ -142,7 +160,7 @@ void Varginha::update(double time_elapsed) {
     instance.verify_collision_with_finish_points(this);
     //Check if the finish point is different from null
     if(finish_point != NULL) {
-        if(finish_point->get_alien_names().find("V") != std::string::npos) {
+        if(finish_point->get_alien_names().find(NAME_CHARACTER) != std::string::npos) {
             in_position = true;
         //Assigns a default in_position true
         }else {
@@ -178,11 +196,11 @@ void Varginha::special_action() {
     // Check is_invisible
     if(is_invisible) {
         // Check if idle_animation_number is equals 5
-        if(idle_animation_number == 5) {
-            animator->set_interval("invisible_right");
+        if(idle_animation_number == ANIMATION_NUMBER_1) {
+            animator->set_interval(INVISIBLE_SPECIAL_RIGHT);
         // If you do not receive an action
         }else {
-            animator->set_interval("invisible_left");
+            animator->set_interval(INVISIBLE_SPECIAL_LEFT);
         }
     // Check is_invisible
     }else if(is_selected) {
@@ -208,12 +226,12 @@ void Varginha::special_action() {
                 }
                  // Check x it's bigger than get_varginha_position_x
                 if(x > get_varginha_position_x()) {
-                    animator->set_interval("action_right");
-                    idle_animation_number = 5;
+                    animator->set_interval(ACTION_SPECIAL_RIGHT);
+                    idle_animation_number = ANIMATION_NUMBER_1;
                     // If you do not receive an action
                     }else {
-                        animator->set_interval("action_left");
-                        idle_animation_number = 0;
+                        animator->set_interval(ACTION_SPECIAL_LEFT);
+                        idle_animation_number = ANIMATION_NUMBER_2;
                     }
                     block_movement = true;
                     turn_off = true;
@@ -224,11 +242,11 @@ void Varginha::special_action() {
         is_invisible = true;
         set_visible(false);
             // Check if animation_number is equals 5
-            if(idle_animation_number == 5) {
-                animator->set_interval("special_right");
+            if(idle_animation_number == ANIMATION_NUMBER_1) {
+                animator->set_interval(SPECIAL_RIGHT);
                // If you do not receive an action
                }else {
-                   animator->set_interval("special_left");
+                   animator->set_interval(SPECIAL_LEFT);
                }
         // If you do not receive an action
         }else {
@@ -271,7 +289,8 @@ void Varginha::draw() {
     double-varginha_position_y = 0.0;
 
     INFO("Varginha DRAW");
-    animator->draw(get_varginha_position_x()-15, get_varginha_position_y()-25);
+    animator->draw(get_varginha_position_x()-CENTER_CHARACTER_1, 
+    get_varginha_position_y()-CENTER_CHARACTER_2);
     animator->draw_collider(get_varginha_position_x(), get_varginha_position_y(), 
     get_width(), get_height());
 }
@@ -288,7 +307,7 @@ void Varginha::verify_turn() {
     bool block_movement = true;
     bool turn_off = true;
     // Check conditions
-    if((timer_turn->elapsed_time()/1000.0) > 0.3 && turn_off) {
+    if((timer_turn->elapsed_time()/1000.0) > TOTAL_TIME_1 && turn_off) {
       block_movement = false;
       turn_off = false;
     }
