1e2135c0c76071169641a904b441f8c8d5f4dace
Emanuel <manusenac@hotmail.com>
2017-10-23 09:36:11 +0000

    Numeros magicos,funcoes atomicas.
    Também foram implementadas estrutura de dados e declarar o mais tarde possível. Sendo que a declaração tardia ja estava implementada no código .

--- game/src/win_scene.cpp
+++ game/src/win_scene.cpp
@@ -3,17 +3,70 @@
 
 #include <typeinfo>
 #include <iostream>
+# define  type def struct SIZE{
+   ROWS
+  COLUMNS
+}
+# define  type def struct Color{
+  int  green;
+  int red;
+  int blue;
+  double alpha;
+  COLUMNS
+}
+#define type  def struct position{
+     int x;
+     int y;
+}3
+#define const  position DRAW.x=80;
+#define const position DRAW.y=20;
+#define const color SELECTCOLOR;
+#define color SELECTCOLOR.red=255;
+#define color SELECTCOLOR.green=255;
+#define color SELECTCOLOR.blue=255;
+#define color SELECTCOLOR.alpha=0.0;
+#define const color SELECTCOLOR;
+# color NOTSELECTCOLOR.red=0;
+#define color NOTSELECTCOLOR.green=0;
+#define color NOTSELECTCOLOR.blue=0;
+#define color NOTSELECTCOLOR.alpha=0.0;
+#define const site ANIMATION;
+#define const ANIMATION.ROWS=1;
+#define const ANIMATION.COLUMNS=5;
+#define const int NOTUPDATE=0;
+# define color ANIMATION.red=158;
+# define color ANIMATION.green=228;    
+# define color ANIMATION.red=158;
+# define color ANIMATION.alpha=125.0;
+#define type def struct SIZEOBJECT{
+    int WiTDTH;
+    int HEIGH ;
+}
+#define type def struct POSITION{
+  Int BEGIN;
+ Int LAST;
+}
+#define const SIZEOBJECT SIZECONTINUE.WITDH=700;
+#define const SIZEOBJECT  SIZECONTINUE.HEIGH=500;
+#define const POSITION POSITIONCONTINUE.BEGIN=500;
+#define const POSITION POSITIONCONTINUE.LAST=500;
 
+#define const SIZEOBJECT SIZEMENU.WITDH=50;
+#define const SIZEOBJECT  SIZEMENU.HEIGH=500;
+#define const POSITION POSITIONMENU.BEGIN=500;
+#define const POSITION POSITIONMENU.LAST=500;
+SIZEMENU,POSITIONMENU
 using namespace engine;
+
 
 WinScene::WinScene(int id) : Scene(id){
     selectButton = 1;
-    select = new Color(255, 255, 255, 0);
-    notSelect = new Color(0, 0, 0, 0);
+    select = new Color( SELECTCOLOR);
+    notSelect = new Color(NOTSELECTCOLOR);
     soundEffect = new Audio("assets/sounds/SELECT6.wav", "EFFECT",100);
-    background = new Animation("assets/sprites/win.png", 1, 5, 0.8);
+    background = new Animation("assets/sprites/win.png", ANIMATION, ANIMATIONTIME);
     background->addAction("win", 0, 4);
-    assert(std::get<animator->list_actions[beignEdited][1]>==0);
+    assert(std::get<animator->list_actions[beignEdited][1]>==NOTUPDATE);
     background->setInterval("win");
     assert(background->interval.first==win);
     
@@ -23,7 +76,7 @@ WinScene::~WinScene(){
 }
 
 void WinScene::draw(){
-        background->draw_instant(80, 20);
+        background->draw_instant(DRAW);
         for(auto gameObject : gameObjectsList) {
                 (*gameObject.second).draw();
         }
@@ -49,33 +102,15 @@ void WinScene::update(double timeElapsed){
 }
 
 void WinScene::load(){
-        gameObjectsList.push_back(std::pair<int, GameObject*>(1,new Button("assets/fonts/font.ttf", 700, 500, 500, 500, "Continue", 50)));
+        gameObjectsList.push_back(std::pair<int, GameObject*>(1,new Button("assets/fonts/font.ttf", SIZECONTINUE,POSITIONCONTINUE, "Continue", 50)));
         assert(get<gameObjectsList.back.[1]==1>);
-        gameObjectsList.push_back(std::pair<int, GameObject*>(2,new Button("assets/fonts/font.ttf", 50, 500, 500, 500, "Menu", 50)));
+        gameObjectsList.push_back(std::pair<int, GameObject*>(2,new Button("assets/fonts/font.ttf",SIZEMENU,POSITIONMENU, "Menu", 50)));
         assert(get<gameObjectsList.back.[1]==2>);
-        AnimationManager::instance.setBackgroundColor(new Color(158,228,159, 125));
+        AnimationManager::instance.setBackgroundColor(new Color(ANIMATION ));
 
 }
 
-void WinScene::selectAction(){
-        if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_LEFT)) {
-                soundEffect->pause();
-                soundEffect->play(0);
-                selectButton--;
-                if(selectButton <= 0) {
-                        selectButton = 2;
-                }
-        }
-        else if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_RIGHT)) {
-                soundEffect->pause();
-                soundEffect->play(0);
-                selectButton++;
-                if(selectButton > 2) {
-                        selectButton = 1;
-                }
-        }
-
-        if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_ENTER)) {
+void  EnterSelect ()  {
                 switch(selectButton) {
                 case 1:
                         if(getSceneManager()->get_before_scene_id() == 1){
@@ -96,6 +131,18 @@ void WinScene::selectAction(){
 
         }
 }
-
+void RightorLeft(){
+  if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_RIGHT)) {
+                selectButton++;
+                if(selectButton > 2) {
+                        selectButton = 1;
+                }
+        }else {
+ if(InputManager::instance.isKeyTriggered(InputManager::KeyPress::KEY_PRESS_LEFT)) {
+                selectButton--;
+                if(selectButton <= 0) {
+                        selectButton = 2;
+                }
+        }
 void WinScene::unload(){
 }
