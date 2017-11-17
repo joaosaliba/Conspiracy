f4ad35fb9a1c4bbe2e91c1cec4f6c35a52d5e915
joaosaliba <joao_salibaa@hotmail.com>
2017-11-17 10:53:12 -0200

    dont let others touch where they should not in arrow button and camera system

--- game/include/button.hpp
+++ game/include/button.hpp
@@ -12,10 +12,14 @@ class Button: public GameObject{
 public:
     Button(std::string button_name, double button_position_x, double button_position_y, int button_width, int button_height, std::string title, int sizeTitle);
     ~Button();
-    void update(double timeElapsed); // update button during time
+
     void setTextColor(Color * newTextColor); // set a color for the button
     void draw(); // make a draw off the button
     Text * getText();
+
+private:
+    void update(double timeElapsed); // update button during time
+
 protected:
     Text * text = nullptr;
 };
