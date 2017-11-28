2bcc1f85c3163c5d1dbac392d247f7e72c6f9a14
Emanuel <manusenac@hotmail.com>
2017-11-28 15:28:03 -0200

    corrigir verificar valores de retorno

--- game/src/lose_scene.cpp
+++ game/src/lose_scene.cpp
@@ -1,6 +1,6 @@
 #include "lose_scene.hpp"
 #include "button.hpp"
-
+#include<assert.h>;
 #include <typeinfo>
 #include <iostream>
 
@@ -11,8 +11,9 @@ LoseScene::LoseScene(int id) : Scene(id){
     select = new Color(255, 255, 255, 0);// seta a cor para seleção 
     notSelect = new Color(0, 0, 0, 0);// seta a cor quando nao ha seleção
     soundEffect = new Audio("assets/sounds/SELECT6.wav", "EFFECT", 100);// seta o efeito do audio
-    backgroundMusic = new Audio("assets/sounds/GAMEOVER.wav", "MUSIC", 50);//seta a musica da cena. 
+    backgroundMusic = new Audio("assets/sounds/GAMEOVER.wav", "MUSIC", 50);//seta a musica da cena.
     background = new Animation("assets/sprites/lose.png", 1, 4, 0.8);// seta a animação para a cena.
+    assert( background.getCurrentPositionFrame()==0);
     background->addAction("lose", 0,3);
     background->setInterval("lose");
 }
@@ -47,9 +48,11 @@ void LoseScene::update(double timeElapsed){
 }
 
 void LoseScene::load(){
-        Button1 button= new Button("assets/fonts/font.ttf", 700, 500, 500, 500, "Continue", 50;  
+        Button1 button= new Button("assets/fonts/font.ttf", 700, 500, 500, 500, "Continue", 50)
+        assert(button.getText().text== "Continue");   
         gameObjectsList.push_back(std::pair<int, GameObject*>(1,button));
-        Button button4 =new Button("assets/fonts/font.ttf", 50, 500, 500, 500, "Menu", 50;
+        Button button4 =new Button("assets/fonts/font.ttf", 50, 500, 500, 500, "Menu", 50);
+        assert(button.getText().text== "Continue");
         gameObjectsList.push_back(std::pair<int, GameObject*>(2,button4));
         AnimationManager::instance.setBackgroundColor(new Color(255,160,156, 125));
         backgroundMusic->play(0);
