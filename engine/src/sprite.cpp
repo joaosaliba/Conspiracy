aa3ae3283c7c4338fe97996b860478ba8d2e01c3
Emanuel <manusenac@hotmail.com>
2017-11-28 13:06:49 -0200

    comentario de linha individual.
    comentario da declaração das variaveis.

--- engine/src/sprite.cpp
+++ engine/src/sprite.cpp
@@ -11,8 +11,8 @@ using namespace engine;
 
     void Sprite::init(){
         INFO("Init sprite.");
-        SDL_Surface * image = NULL;
-        image = IMG_Load(directory.c_str());
+        SDL_Surface * image = NULL;// delcara imagem como vazia
+        image = IMG_Load(directory.c_str());// carrega a imagem
 
         if(image == NULL){
             ERROR("INIT SPRITE ERROR.");
@@ -26,8 +26,8 @@ using namespace engine;
             exit(-1);
         }
 
-        drawWidth = lenght.first = image->w;
-        drawHeight = lenght.second = image->h;
+        drawWidth = lenght.first = image->w;// seta a altura da primeira imagem
+        drawHeight = lenght.second = image->h;// seta a latura da segunda imagem.
         SDL_FreeSurface(image);
 
     }
@@ -35,15 +35,15 @@ using namespace engine;
     void Sprite::shutdown(){
         INFO("Destroy sprite.");
         SDL_DestroyTexture(texture);
-        texture = NULL;
+        texture = NULL;// termina o metodo detruindo a imagem
     }
     void Sprite::setDrawSize(int w, int h){
-        drawWidth = w;
-        drawHeight = h;
+        drawWidth = w;// seta a largura
+        drawHeight = h;// seta a altura.
     }
     void Sprite::draw(int x, int y){
-        axis.first = x;
-        axis.second = y;
+        axis.first = x;// seta o eixo horizontal.
+        axis.second = y;// seta o eixo vertical.
          //Crop image
         clipRect = {0, 0, lenght.first, lenght.second};
 
