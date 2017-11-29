a9be5aabdfa63786829c9b4085e9dd6dd4aa5c66
Emanuel <manusenac@hotmail.com>
2017-11-29 15:48:59 -0200

    verificar limites numericos
    verificar limites numericos relacionados a animação

--- game/src/table.cpp
+++ game/src/table.cpp
@@ -2,12 +2,28 @@
 #include "table.hpp"
 #include "log.h"
 #include <assert.h>
+#define const MAX_COLUMS=1000;
+#define const MAX_LINES=1000;
+#define const LINES=1;
+#define const COLUMS=1;
+#define const TIME=0.5;
+
+Bool test_animation(int colums,int lines){
+     Bool valida;
+     if(colums<MAX_COLUMS){
+       if(lines>MAX_LINES){
+         valida=TRUE;
+       }else{
+         valida=FALSE;
+     }else{
+         valida=FALSE;
+}         
 Table::Table(std::string objectName, double positionX, double positionY,
                                      int width, int height) : GameObject(objectName,
                                                                          positionX,
                                                                          positionY,
-                                                                         width, height){
-animator = new Animation(objectName, 1, 1, 0.5);
+                                                                width, height){
+animator = new Animation(objectName, LINES,COLUMS,TIME);
 assert( background.getCurrentPositionFrame()==0);
 }
 
