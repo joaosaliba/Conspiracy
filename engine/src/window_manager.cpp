d426e96da04205750e3bf8e3d17d407a8cc90d09
Emanuel <manusenac@hotmail.com>
2017-11-29 11:50:10 -0200

    codigo simples com bo a apresentação
    separação dos metodos de teste de erro , do fluxo principal.

--- engine/src/window_manager.cpp
+++ engine/src/window_manager.cpp
@@ -3,24 +3,35 @@ using namespace engine;
 
 SDL_Window *gameWindow;
 SDL_Renderer *gameCanvas;
-
+void test_game_window(){
+    if(gameWindow == NULL){
+        return false;
+    }
+}
+ void test game_canvas(){
+  
+   if(game_canvas == NULL){
+            return false;
+        }
+ }
+void     test_image(){
+      if(image == NULL){
+            exit(-1);
+      }
+}
 bool WindowManager::createWindow(std::string windowTitle, std::pair<int, int> windowSize){
     INFO("Create window");
     gameWindow = SDL_CreateWindow(windowTitle.c_str(), SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED, windowSize.first,
     windowSize.second, SDL_WINDOW_SHOWN);
 
-    if(gameWindow == NULL){
-        return false;
-    }
+    test_game_windown();
 
     INFO("Create canvas");
     game_canvas = SDL_CreateRenderer(gameWindow, -1,
         SDL_RENDERER_ACCELERATED);
 
-        if(game_canvas == NULL){
-            return false;
-        }
+        test_game_canvas()
 
         SDL_SetRenderDrawColor(game_canvas, 100, 100, 100, 255);
         SDL_RenderClear(game_canvas);
@@ -51,15 +62,13 @@ bool WindowManager::createWindow(std::string windowTitle, std::pair<int, int> wi
         return game_canvas;
     }
     
+
     void WindowManager::setImageIcon(){
       // TODO Change image icon.
       SDL_Surface * image = NULL;
       image = IMG_Load(std::string("assets/sprites/alien.png").c_str());
-
-      if(image == NULL){
-            exit(-1);
-      }
-
+      test_image()     
+      
       SDL_SetWindowIcon(game_window, image);
       SDL_FreeSurface(image);
 
