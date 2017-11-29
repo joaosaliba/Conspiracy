08bd61d82068e9e5c9e392b63e5af062f545647b
Emanuel <manusenac@hotmail.com>
2017-11-29 14:58:13 -0200

    tipos descritivos
    criação de um novo tipo descritivo voltado para exibição

--- game/src/table.cpp
+++ game/src/table.cpp
@@ -1,12 +1,20 @@
 #include "table.hpp"
 #include "log.h"
+type def struct Showing{
+      int position x;
+      int position y;
+      int time;
+} 
 
 Table::Table(std::string objectName, double positionX, double positionY,
                                      int width, int height) : GameObject(objectName,
-                                                                         positionX,
-                                                                         positionY,
-                                                                         width, height){
-animator = new Animation(objectName, 1, 1, 0.5);
+                                                                         positionX,positionY,  width, height){
+Showing table.positionx= 1;
+table.position_y=  1;
+table.time=0.5;
+                                                                       
+animator = new Animation(objectName,table.position_x,table.position_y,
+table.time );
 }
 
 Table::~Table(){
